b785d6a69b4c232ebb0411812fb95c6c749505cc
sopigoo <shofinadhifa24@gmail.com>
2024-08-28 16:27:24 +0700

    update allowed host

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5_m__^3i0nh^&omxue3p87kn=d!uv4^$w02a$=d6gxl+o$=wd1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "siti-shofi-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
